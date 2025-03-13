4ae0d9f26db031fd3cc09cf9839dd7b3919a7229
Unicorn_galaxy <1366131367@qq.com>
2025-03-13 09:34:30 +0800

    第三周作业提交

---
+++ Homework3/Clock.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Clock
+{
+    // 自定义事件参数类
+    public class TickEventArgs : EventArgs
+    {
+        public DateTime CurrentTime { get; set; }
+    }
+
+    public class AlarmEventArgs : EventArgs
+    {
+        public DateTime AlarmTime { get; set; }
+    }
+
+    // 闹钟类
+    public class AlarmClock
+    {
+        public event EventHandler<TickEventArgs> Tick;
+        public event EventHandler<AlarmEventArgs> Alarm;
+
+        private DateTime? _alarmTime;
+        private readonly Timer _timer;
+
+        public AlarmClock()
+        {
+            _timer = new Timer(1000); // 1秒间隔
+            _timer.Elapsed += TimerElapsed;
+            _timer.AutoReset = true;
+            _timer.Start();
+        }
+
+        public void SetAlarm(DateTime alarmTime)
+        {
+            _alarmTime = alarmTime;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            var now = DateTime.Now;
+
+            // 触发Tick事件
+            Tick?.Invoke(this, new TickEventArgs { CurrentTime = now });
+
+            // 检查并触发Alarm事件
+            if (_alarmTime.HasValue && now >= _alarmTime.Value)
+            {
+                Alarm?.Invoke(this, new AlarmEventArgs { AlarmTime = _alarmTime.Value });
+                _alarmTime = null; // 重置闹钟
+            }
+        }
+    }
+
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,33 @@
+﻿using Clock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alarmClock = new AlarmClock();
+
+            // 订阅事件
+            alarmClock.Tick += (s, e) =>
+                Console.WriteLine($"滴答... 当前时间：{e.CurrentTime:HH:mm:ss}");
+
+            alarmClock.Alarm += (s, e) =>
+                Console.WriteLine($"叮铃铃！！！闹钟时间到：{e.AlarmTime:HH:mm:ss}");
+
+            Console.WriteLine("按任意键退出...");
+            // 设置5秒后的闹钟
+            var alarmTime = DateTime.Now.AddSeconds(10);
+            Console.WriteLine($"设置闹钟时间：{alarmTime:HH:mm:ss}");
+            alarmClock.SetAlarm(alarmTime);
+
+            
+            Console.ReadKey();
+        }
+    }
+}
