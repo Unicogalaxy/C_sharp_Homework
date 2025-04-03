0ffde5569f2778679b4ba2e7a8e0f537116d789e
Unicorn_galaxy <1366131367@qq.com>
2025-04-03 13:03:34 +0800

    第六周作业

---
+++ Homework5/Week6_test1/Week6_test1/MyForm.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace Week6_test1
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel3);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(685, 1012);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // button16
+            // 
+            this.button16.Font = new System.Drawing.Font("微软雅黑", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button16.Location = new System.Drawing.Point(506, 506);
+            this.button16.Margin = new System.Windows.Forms.Padding(8);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(150, 150);
+            this.button16.TabIndex = 15;
+            this.button16.Text = "/";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button15
+            // 
+            this.button15.Font = new System.Drawing.Font("微软雅黑", 25.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button15.Location = new System.Drawing.Point(340, 506);
+            this.button15.Margin = new System.Windows.Forms.Padding(8);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(150, 150);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "*";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("微软雅黑", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button14.Location = new System.Drawing.Point(174, 506);
+            this.button14.Margin = new System.Windows.Forms.Padding(8);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(150, 150);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "-";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button13
+            // 
+            this.button13.Font = new System.Drawing.Font("微软雅黑", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button13.Location = new System.Drawing.Point(8, 506);
+            this.button13.Margin = new System.Windows.Forms.Padding(8);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(150, 150);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "+";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("微软雅黑", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button12.Location = new System.Drawing.Point(506, 340);
+            this.button12.Margin = new System.Windows.Forms.Padding(8);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(150, 150);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "DEL";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("微软雅黑", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button11.Location = new System.Drawing.Point(340, 340);
+            this.button11.Margin = new System.Windows.Forms.Padding(8);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(150, 150);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "=";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("幼圆", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button10.Location = new System.Drawing.Point(174, 340);
+            this.button10.Margin = new System.Windows.Forms.Padding(8);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(150, 150);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "9";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("幼圆", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button9.Location = new System.Drawing.Point(8, 340);
+            this.button9.Margin = new System.Windows.Forms.Padding(8);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(150, 150);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "8";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("幼圆", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button8.Location = new System.Drawing.Point(506, 174);
+            this.button8.Margin = new System.Windows.Forms.Padding(8);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(150, 150);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "7";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("幼圆", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.Location = new System.Drawing.Point(340, 174);
+            this.button7.Margin = new System.Windows.Forms.Padding(8);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 150);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("幼圆", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button6.Location = new System.Drawing.Point(174, 174);
+            this.button6.Margin = new System.Windows.Forms.Padding(8);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 150);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("幼圆", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.Location = new System.Drawing.Point(8, 174);
+            this.button5.Margin = new System.Windows.Forms.Padding(8);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 150);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("幼圆", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(506, 8);
+            this.button4.Margin = new System.Windows.Forms.Padding(8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 150);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("幼圆", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(340, 8);
+            this.button3.Margin = new System.Windows.Forms.Padding(8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 150);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("幼圆", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(174, 8);
+            this.button2.Margin = new System.Windows.Forms.Padding(8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 150);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("幼圆", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(8, 8);
+            this.button1.Margin = new System.Windows.Forms.Padding(8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 150);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.button1);
+            this.flowLayoutPanel3.Controls.Add(this.button2);
+            this.flowLayoutPanel3.Controls.Add(this.button3);
+            this.flowLayoutPanel3.Controls.Add(this.button4);
+            this.flowLayoutPanel3.Controls.Add(this.button5);
+            this.flowLayoutPanel3.Controls.Add(this.button6);
+            this.flowLayoutPanel3.Controls.Add(this.button7);
+            this.flowLayoutPanel3.Controls.Add(this.button8);
+            this.flowLayoutPanel3.Controls.Add(this.button9);
+            this.flowLayoutPanel3.Controls.Add(this.button10);
+            this.flowLayoutPanel3.Controls.Add(this.button11);
+            this.flowLayoutPanel3.Controls.Add(this.button12);
+            this.flowLayoutPanel3.Controls.Add(this.button13);
+            this.flowLayoutPanel3.Controls.Add(this.button14);
+            this.flowLayoutPanel3.Controls.Add(this.button15);
+            this.flowLayoutPanel3.Controls.Add(this.button16);
+            this.flowLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 218);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(668, 809);
+            this.flowLayoutPanel3.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("幼圆", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(665, 209);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.flowLayoutPanel2.Controls.Add(this.textBox1);
+            this.flowLayoutPanel2.Font = new System.Drawing.Font("幼圆", 22.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(668, 209);
+            this.flowLayoutPanel2.TabIndex = 0;
+            this.flowLayoutPanel2.WrapContents = false;
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 1012);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "MyForm";
+            this.Text = "MyForm";
+            this.Load += new System.EventHandler(this.MyForm_Load);
+            this.SizeChanged += new System.EventHandler(this.MyForm_SizeChanged);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+    }
+}
---
+++ Homework5/Week6_test1/Week6_test1/MyForm.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Windows.Forms;
+using AutoWindowsSize;
+
+namespace Week6_test1
+{
+    public partial class MyForm : Form
+    {
+        AutoAdaptWindowsSize AutoSize;
+        private string currentInput = "";
+        private double? firstNumber = null;
+        private string currentOperator = "";
+        private bool newInput = true;
+        private string expression = ""; // 新增变量用于存储完整表达式
+
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        // 按下按钮后文本框显示的变化
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // 按下数字按钮时的通用处理方法
+        private void AppendNumber(string number)
+        {
+            if (newInput)
+            {
+                currentInput = number;
+                newInput = false;
+            }
+            else
+            {
+                currentInput += number;
+            }
+            if (string.IsNullOrEmpty(expression))
+            {
+                expression = currentInput;
+            }
+            else if (string.IsNullOrEmpty(currentOperator))
+            {
+                expression = expression.Replace(currentInput, "") + currentInput;
+            }
+            else
+            {
+                expression += number;
+            }
+            textBox1.Text = expression;
+        }
+
+        // 按下 0 对应的操作
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AppendNumber("0");
+        }
+
+        // 按下 1 对应的操作
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AppendNumber("1");
+        }
+
+        // 按下 2 对应的操作
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AppendNumber("2");
+        }
+
+        // 按下 3 对应的操作
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AppendNumber("3");
+        }
+
+        // 按下 4 对应的操作
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AppendNumber("4");
+        }
+
+        // 按下 5 对应的操作
+        private void button6_Click(object sender, EventArgs e)
+        {
+            AppendNumber("5");
+        }
+
+        // 按下 6 对应的操作
+        private void button7_Click(object sender, EventArgs e)
+        {
+            AppendNumber("6");
+        }
+
+        // 按下 7 对应的操作
+        private void button8_Click(object sender, EventArgs e)
+        {
+            AppendNumber("7");
+        }
+
+        // 按下 8 对应的操作
+        private void button9_Click(object sender, EventArgs e)
+        {
+            AppendNumber("8");
+        }
+
+        // 按下 9 对应的操作
+        private void button10_Click(object sender, EventArgs e)
+        {
+            AppendNumber("9");
+        }
+
+        // 按下 = 对应的操作
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (firstNumber.HasValue && !string.IsNullOrEmpty(currentOperator) && !string.IsNullOrEmpty(currentInput))
+            {
+                double secondNumber = double.Parse(currentInput);
+                double result = 0;
+                switch (currentOperator)
+                {
+                    case "+":
+                        result = firstNumber.Value + secondNumber;
+                        break;
+                    case "-":
+                        result = firstNumber.Value - secondNumber;
+                        break;
+                    case "*":
+                        result = firstNumber.Value * secondNumber;
+                        break;
+                    case "/":
+                        if (secondNumber != 0)
+                        {
+                            result = firstNumber.Value / secondNumber;
+                        }
+                        else
+                        {
+                            textBox1.Text = "Error: Division by zero";
+                            return;
+                        }
+                        break;
+                }
+                expression = $"{firstNumber.Value}{currentOperator}{secondNumber}={result}";
+                textBox1.Text = expression;
+                currentInput = result.ToString();
+                firstNumber = null;
+                currentOperator = "";
+                newInput = true;
+            }
+        }
+
+        // 按下 del 对应的操作
+        private void button12_Click(object sender, EventArgs e)
+        {
+            currentInput = "";
+            firstNumber = null;
+            currentOperator = "";
+            newInput = true;
+            expression = "";
+            textBox1.Text = "";
+        }
+
+        // 按下运算符按钮时的通用处理方法
+        private void SetOperator(object sender, EventArgs e, string op)
+        {
+            if (!string.IsNullOrEmpty(currentInput))
+            {
+                if (firstNumber == null)
+                {
+                    firstNumber = double.Parse(currentInput);
+                }
+                else
+                {
+                    button11_Click(sender, e);
+                    firstNumber = double.Parse(currentInput);
+                }
+                currentOperator = op;
+                newInput = true;
+                expression += op;
+                textBox1.Text = expression;
+            }
+        }
+
+        // 按下 + 对应的操作
+        private void button13_Click(object sender, EventArgs e)
+        {
+            SetOperator(sender, e, "+");
+        }
+
+        // 按下 - 对应的操作
+        private void button14_Click(object sender, EventArgs e)
+        {
+            SetOperator(sender, e, "-");
+        }
+
+        // 按下 * 对应的操作
+        private void button15_Click(object sender, EventArgs e)
+        {
+            SetOperator(sender, e, "*");
+        }
+
+        // 按下 / 对应的操作
+        private void button16_Click(object sender, EventArgs e)
+        {
+            SetOperator(sender, e, "/");
+        }
+
+        private void MyForm_Load(object sender, EventArgs e)
+        {
+            AutoSize = new AutoAdaptWindowsSize(this);
+        }
+
+        private void MyForm_SizeChanged(object sender, EventArgs e)
+        {
+            if (AutoSize != null) // 一定加这个判断，电脑缩放布局不是100%的时候，会报错
+            {
+                AutoSize.FormSizeChanged();
+            }
+        }
+    }
+}
